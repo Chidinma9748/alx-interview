5db447aacbcad352aff5582076b3087dee70158a
Chidinma9748 <chidinmaukachi20@gmail.com>
2024-10-04 09:55:09 +0000

    corrected identations

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,23 +1,24 @@
 #!/usr/bin/python3
 def pascal_triangle(n):
     if n <= 0:
-        return[]
+        return []
+    
+    # Initialize the first row of Pascal's Triangle
+    triangle = [[1]]
+    
+    for i in range(1, n):
+        # Start each row with 1
+        row = [1]
+        
+        # Calculate the values for the middle elements of the row
+        for j in range(1, i):
+            row.append(triangle[i-1][j-1] + triangle[i-1][j])
+        
+        # End each row with 1
+        row.append(1)
+        
+        # Add the row to the triangle
+        triangle.append(row)
+    
+    return triangle
 
-    """Initializing the first row of the Pascals triangle"""
-    triangle - [[1]]
-
-     for i in range(1, n):
-         """each row starts with 1"""
-         row = [1]
-
-         """calculate the middle numbers"""
-         for j in range(1, i):
-             row.append(triangle[i-1][j-1] + triangle[i-1][j])
-
-             """end each row with 1"""
-         row.append(1)
-
-         """add row to the triangle"""
-         triangle.append(row)
-
-     return triangle
