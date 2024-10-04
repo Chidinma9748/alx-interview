03ed7ead8839d44ff1077e22f2abe7ccc391b598
Chidinma9748 <chidinmaukachi20@gmail.com>
2024-10-04 09:26:49 +0000

    Pascals triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+def pascal_triangle(n):
+     if n <= 0
+        return[]
+
+    """Initializing the first row of the Pascals triangle"""
+    triangle - [[1]]
+
+     for i in range(1, n):
+         """each row starts with 1"""
+         row = [1]
+
+         """calculate the middle numbers"""
+         for j in range(1, i):
+             row.append(triangle[i-1][j-1] + triangle[i-1][j])
+
+             """end each row with 1"""
+         row.append(1)
+
+         """add row to the triangle"""
+         triangle.append(row)
+
+     return triangle
---
+++ 0x00-pascal_triangle/README.md
@@ -0,0 +1,1 @@
+This involves creating a function that implements the pascal's triangle
